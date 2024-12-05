6dc64c66deabef96dcc5089e49bde3fbc38368e0
Azuzi <azuzii@proton.me>
2024-12-05 21:25:59 +0100

    Created UserModule Registered IUser

---
+++ src/user/user.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { IUser } from './user.interface';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UserController],
+  providers: [
+    UserService,
+    {
+      provide: IUser,
+      useExisting: UserService,
+    },
+  ],
+})
+export class UserModule {}
