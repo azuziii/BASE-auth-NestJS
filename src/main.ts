cb0e7ae243733eb5dd8a92308577a63c1e7fd995
Azuzi <azuzii@proton.me>
2024-12-05 22:49:08 +0100

    Using CookieParser middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as CookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //  add secrete
+  app.use(CookieParser());
   await app.listen(3000);
 }
 bootstrap();
