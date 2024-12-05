ee725f8d553d9625864eb6cf1f59b13fccb0ee10
Azuzi <azuzii@proton.me>
2024-12-05 21:05:06 +0100

    Created Base service

---
+++ src/base/base.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  DeepPartial,
+  FindOptionsOrder,
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+import { Base } from './base.entity';
+
+@Injectable()
+export class BaseService<
+  T extends Base,
+  CreateDto extends DeepPartial<T>,
+  UpdateDto extends QueryDeepPartialEntity<T>,
+> {
+  constructor(protected repository: Repository<T>) {}
+
+  async create(createDto: CreateDto): Promise<T> {
+    return this.repository.create(createDto);
+  }
+
+  async save(createDto: CreateDto) {
+    const entity = await this.create(createDto);
+    return this.repository.save(entity);
+  }
+
+  findAll(
+    whereOptions?: FindOptionsWhere<T>,
+    orderOptions?: FindOptionsOrder<T>,
+  ): Promise<T[]> {
+    return this.repository.find({ where: whereOptions, order: orderOptions });
+  }
+
+  async findOne(
+    whereOptions?: FindOptionsWhere<T>,
+    { withException = false } = {},
+  ): Promise<T> {
+    const found = await this.repository.findOne({ where: whereOptions });
+    if (!found && withException) {
+      throw new NotFoundException();
+    }
+
+    return found;
+  }
+
+  async update(id: string, updateDto: UpdateDto) {
+    await this.findOne({ id } as FindOptionsWhere<T>);
+    return this.repository.update(id, updateDto);
+  }
+
+  async remove(id: string) {
+    const found = await this.findOne({ id } as FindOptionsWhere<T>);
+    return this.repository.remove(found);
+  }
+}
