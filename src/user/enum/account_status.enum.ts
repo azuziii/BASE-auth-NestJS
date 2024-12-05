64fa63e394398eab463efc015a21b81083690e3c
Azuzi <azuzii@proton.me>
2024-12-05 21:16:16 +0100

    Created User enums

---
+++ src/user/enum/account_status.enum.ts
@@ -0,0 +1,7 @@
+export enum AccountStatus {
+  ACTIVE = 'ACTIVE',
+  SUSPENDED = 'SUSPENDED',
+  UNVERIFIED = 'UNVERIFIED',
+  DEACTIVATED = 'DEACTIVATED',
+  BANNED = 'BANNED',
+}
---
+++ src/user/enum/profile_visibility.enum.ts
@@ -0,0 +1,5 @@
+export enum ProfileVisibility {
+  PUBLIC = 'PUBLIC',
+  FRIENDS = 'FRIENDS',
+  PRIVATE = 'PRIVATE ',
+}
