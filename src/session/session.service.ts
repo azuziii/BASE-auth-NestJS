855ed8579dff4bbf0c6816bb4669a576946f1a06
Azuzi <azuzii@proton.me>
2024-12-05 22:52:06 +0100

    Created session service

---
+++ src/session/session.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { CreateSessionDto } from './dto/create-session.dto';
+import { BaseService } from 'src/shared/base/base.service';
+import { Session } from './entities/session.entity';
+import * as crypto from 'crypto';
+import { User } from 'src/user/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ClsService } from 'nestjs-cls';
+import { USER_AGENT_SYMBOL, USER_IP_SYMBOL } from 'src/user/constants';
+
+@Injectable()
+export class SessionService extends BaseService<
+  Session,
+  CreateSessionDto,
+  never
+> {
+  constructor(
+    @InjectRepository(Session)
+    protected readonly repository: Repository<Session>,
+    private readonly cls: ClsService,
+  ) {
+    super(repository);
+  }
+
+  generate(): string {
+    return crypto.randomBytes(16).toString('base64');
+  }
+
+  genereateAndSave(user: User) {
+    const token = this.generate();
+
+    this.save({
+      expires_at: new Date(Date.now() + 3600 * 24 * 30),
+      ip: this.cls.get(USER_IP_SYMBOL),
+      user_agent: this.cls.get(USER_AGENT_SYMBOL),
+      session_token: token,
+      user,
+    });
+  }
+}
