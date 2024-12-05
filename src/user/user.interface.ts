08843de3445d1336d8b96a1809564dff99c81e69
Azuzi <azuzii@proton.me>
2024-12-05 22:50:11 +0100

    Added findOne method to user interface

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,6 +1,8 @@
+import { FindOptionsWhere } from 'typeorm';
 import { User } from './entities/user.entity';
 
 export const IUser = 'IUser';
 export interface IUser {
+  findOne(whereOptions?: FindOptionsWhere<User>): Promise<User>;
   findByUsername(username: string): Promise<User>;
 }
