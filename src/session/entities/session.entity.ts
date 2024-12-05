8ac4ebc2ad8f3853ce523099054aa0d0b2bad67d
Azuzi <azuzii@proton.me>
2024-12-05 22:51:32 +0100

    Created Session entity

---
+++ src/session/entities/session.entity.ts
@@ -0,0 +1,25 @@
+import { Base } from 'src/shared/base/base.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Column, ManyToOne } from 'typeorm';
+
+export class Session extends Base {
+  @Column({ nullable: false, type: 'text', unique: true })
+  session_token!: string;
+
+  @Column({ nullable: false, type: 'timestamptz' })
+  expires_at!: Date;
+
+  @Column({ nullable: false, type: 'text' })
+  ip!: string;
+
+  @Column({ nullable: false, type: 'text' })
+  user_agent!: string;
+
+  @ManyToOne(() => User, (user) => user.sessions, { eager: true })
+  user!: User;
+
+  isActive() {
+    const now = new Date();
+    return now <= this.expires_at;
+  }
+}
