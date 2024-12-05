1ac7a0d5dce8ccac03d6b8f8a08bdd020c4ead9d
Azuzi <azuzii@proton.me>
2024-12-05 23:58:42 +0100

    Chore: Register Session entity in TypeOrmModule

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Session } from './entities/session.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Session])],
   providers: [SessionService],
 })
 export class SessionModule {}
