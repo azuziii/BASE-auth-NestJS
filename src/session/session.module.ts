484243b6f93171f22eb19fac2b781e9be8e5dd3a
Azuzi <azuzii@proton.me>
2024-12-07 20:22:28 +0100

    Registered ISession and SessionService

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Session } from './entities/session.entity';
+import { ISession } from './session.interface';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Session])],
-  providers: [SessionService],
+  providers: [
+    SessionService,
+    {
+      provide: ISession,
+      useExisting: SessionService,
+    },
+  ],
+  exports: [ISession],
 })
 export class SessionModule {}
