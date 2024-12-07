cdf7a247046e4adb823aed0c2546419142547c88
Azuzi <azuzii@proton.me>
2024-12-07 20:24:23 +0100

    Added save and findByEmail to user interface

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,8 +1,11 @@
 import { FindOptionsWhere } from 'typeorm';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 export const IUser = 'IUser';
 export interface IUser {
+  save(createDto: CreateUserDto): Promise<User>;
   findOne(whereOptions?: FindOptionsWhere<User>): Promise<User>;
   findByUsername(username: string): Promise<User>;
+  findByEmail(email: string): Promise<User>;
 }
