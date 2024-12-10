5ce4d7bdb71215e7dd3880b9ad2dde35674ee7a2
Azuzi <azuzii@proton.me>
2024-12-10 23:01:03 +0100

    Added session_token and type columns Added used method

--- src/core/otp/entities/otp.entity.ts
+++ src/core/otp/entities/otp.entity.ts
@@ -1,6 +1,7 @@
 import { Base } from 'src/shared/base/base.entity';
 import { User } from 'src/user/entities/user.entity';
 import { BeforeInsert, Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { OtpType } from '../enums/otp-type.enum';
 
 @Entity('otp')
 export class Otp extends Base {
@@ -12,6 +13,12 @@ export class Otp extends Base {
 
   @Column({ nullable: false, type: 'text' })
   user_agent!: string;
+
+  @Column({ nullable: false, type: 'text' })
+  session_token!: string;
+
+  @Column({ nullable: false, enum: OtpType })
+  type!: OtpType;
 
   @ManyToOne(() => User)
   @JoinColumn()
@@ -33,5 +40,9 @@ export class Otp extends Base {
   isExpired() {
     const now = new Date();
     return now.getTime() > this.expires_at.getTime();
+  }
+
+  used() {
+    this.is_used = true;
   }
 }
