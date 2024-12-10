fc589d9f6cc8c7fba3973dfada63ef9a58b430d4
Azuzi <azuzii@proton.me>
2024-12-10 22:57:59 +0100

    Added username to string

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ClsService } from 'nestjs-cls';
+import { USER_SYMBOL } from './user/constants';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly cls: ClsService) {}
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello ' + this.cls.get(USER_SYMBOL).username;
   }
 }
