e8e6f7670e9cd2d04aa36d18dc2826d3a64d6a50
Azuzi <azuzii@proton.me>
2024-12-10 23:00:11 +0100

    Added verifyOtp and logout methods Fix: Saving session token when user logs in or registers Fix: Generating sessionToken before sending OTP

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { User } from 'src/user/entities/user.entity';
 import { IUser } from 'src/user/user.interface';
 import * as bcrypt from 'bcrypt';
@@ -6,13 +6,19 @@ import { ISession } from 'src/session/session.interface';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 import { IAuth } from './auth.interface';
-import { ResponseDto } from 'src/core/types/response.dto';
+import { IOtp } from 'src/core/otp/otp.interface';
+import { OtpType } from 'src/core/otp/enums/otp-type.enum';
+import { ClsService } from 'nestjs-cls';
+import { USER_SYMBOL } from 'src/user/constants';
+import { SESSION_TOKEN } from 'src/session/constants';
 
 @Injectable()
 export class AuthService implements IAuth {
   constructor(
+    private readonly cls: ClsService,
     @Inject(IUser) private readonly userService: IUser,
     @Inject(ISession) private readonly sessionService: ISession,
+    @Inject(IOtp) private readonly otpService: IOtp,
   ) {}
 
   async login({ username }: LoginDto): Promise<string> {
@@ -21,14 +27,12 @@ export class AuthService implements IAuth {
       throw new BadRequestException('Username or password is incorrect');
     }
     const sessionTokn = await this.sessionService.genereateAndSave(user);
+    this.cls.set(SESSION_TOKEN, sessionTokn.session_token);
+
     return sessionTokn.session_token;
   }
 
-  async register({
-    email,
-    username,
-    password,
-  }: RegisterDto): Promise<ResponseDto> {
+  async register({ email, username, password }: RegisterDto): Promise<string> {
     const isUsernameTaken = await this.userService.findByUsername(username);
     if (isUsernameTaken) {
       throw new BadRequestException('Username taken');
@@ -41,15 +45,42 @@ export class AuthService implements IAuth {
 
     const hashedPassword = await this.hashPassword(password);
 
-    await this.userService.save({
+    const user = await this.userService.save({
       email,
       username,
       password: hashedPassword,
     });
 
-    return { message: 'Please check your email to confirm your registration.' };
+    this.cls.set(USER_SYMBOL, user);
 
-    return this.login({ username, password });
+    const sessionToken = await this.login({ username, password });
+
+    await this.otpService.saveAndSend(OtpType.ACTIVATE_ACCOUNT);
+
+    return sessionToken;
+  }
+
+  async logout() {
+    const sessionToken = this.cls.get<string>(SESSION_TOKEN);
+    await this.sessionService.invalidate(sessionToken);
+    return '';
+  }
+
+  async verifyOTP(otp: string): Promise<void> {
+    const isValid = await this.otpService.verify(otp, OtpType.ACTIVATE_ACCOUNT);
+
+    if (!isValid) {
+      // fix
+      throw new BadRequestException('Bad otp');
+    }
+
+    const user = this.cls.get<User>(USER_SYMBOL);
+
+    user.setAccountVerified();
+
+    await this.userService.save(user);
+
+    return;
   }
 
   async hashPassword(password: string): Promise<string> {
@@ -59,7 +90,7 @@ export class AuthService implements IAuth {
   }
 
   async validateUser(username: string, password: string): Promise<User> {
-    const user = await this.userService.findOne({ username });
+    const user = await this.userService.findOne({ where: { username } });
     if (user) {
       const compare = await bcrypt.compare(password, user.password);
       if (!compare) return null;
