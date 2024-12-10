7381817a5f1bb219fcec9da34ab51a7620e168bc
Azuzi <azuzii@proton.me>
2024-12-10 23:01:29 +0100

    Added session_token and type properties

--- src/core/otp/dto/create-otp.dto.ts
+++ src/core/otp/dto/create-otp.dto.ts
@@ -1,16 +1,28 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEnum, IsIP, IsNotEmpty, IsString } from 'class-validator';
 import { User } from 'src/user/entities/user.entity';
+import { OtpType } from '../enums/otp-type.enum';
 
 export class CreateOtpDto {
   @IsNotEmpty()
+  @IsString()
   otp!: string;
 
   @IsNotEmpty()
+  @IsIP()
   ip!: string;
 
   @IsNotEmpty()
+  @IsString()
   user_agent!: string;
 
   @IsNotEmpty()
+  @IsString()
+  session_token!: string;
+
+  @IsNotEmpty()
   user!: User;
+
+  @IsNotEmpty()
+  @IsEnum(OtpType)
+  type!: OtpType;
 }
