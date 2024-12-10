294bb98fc4cc14697cbef5b623f06b4045f7202c
Azuzi <azuzii@proton.me>
2024-12-10 22:54:09 +0100

    Added isVerified Updated findOne parameter

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,11 +1,12 @@
-import { FindOptionsWhere } from 'typeorm';
+import { FindOneOptions } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 
 export const IUser = 'IUser';
 export interface IUser {
   save(createDto: CreateUserDto): Promise<User>;
-  findOne(whereOptions?: FindOptionsWhere<User>): Promise<User>;
+  findOne(options: FindOneOptions<User>): Promise<User>;
   findByUsername(username: string): Promise<User>;
   findByEmail(email: string): Promise<User>;
+  isVerified(): boolean;
 }
