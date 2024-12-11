4c251529626d728580362f735bf81c97f718302a
Azuzi <azuzii@proton.me>
2024-12-11 14:31:25 +0100

    Registered SessionGuard and SessionInterceptor

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -3,6 +3,9 @@ import { SessionService } from './session.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Session } from './entities/session.entity';
 import { ISession } from './session.interface';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { SessionInterceptor } from './interceptor/session.interceptor';
+import { SessionGuard } from './guards/session.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Session])],
@@ -11,6 +14,14 @@ import { ISession } from './session.interface';
     {
       provide: ISession,
       useExisting: SessionService,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: SessionInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: SessionGuard,
     },
   ],
   exports: [ISession],
