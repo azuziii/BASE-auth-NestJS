446d4924b51e06d63595be9fdc26b3da4d93397b
Azuzi <azuzii@proton.me>
2024-12-11 14:31:31 +0100

    Created SessionGuard

---
+++ src/session/guards/session.guard.ts
@@ -0,0 +1,34 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+import { Request } from 'express';
+import { PUBLIC } from 'src/shared/decorators/public.decorator';
+import { SESSION_TOKEN } from 'src/session/constants';
+import { ISession } from 'src/session/session.interface';
+
+@Injectable()
+export class SessionGuard implements CanActivate {
+  constructor(@Inject(ISession) private readonly sessionService: ISession) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const isPublic = Reflect.getMetadata(PUBLIC, context.getHandler());
+
+    if (isPublic) return true;
+
+    const request: Request = context.switchToHttp().getRequest();
+
+    const sessionToken = request.cookies[SESSION_TOKEN];
+    console.log(sessionToken);
+
+    if (!sessionToken) return false;
+
+    const user = request.user as User;
+    console.log(user);
+
+    return this.sessionService.verify(sessionToken);
+  }
+}
